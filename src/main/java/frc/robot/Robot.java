7e20fe88ce22edeaa6cf13ce3d979845d77f2070
losClasicos5278 <152567506+losClasicos5278@users.noreply.github.com>
2024-02-15 19:02:35 -0600

    code cleanup/formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -15,22 +16,19 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.controller.PIDController;
 
-
-
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  
-  // private static WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(3);
-  // private static WPI_TalonSRX leftBackMotor = new WPI_TalonSRX(4);
-  // private static WPI_TalonSRX rightFrontMotor = new WPI_TalonSRX(1);
-  // private static WPI_TalonSRX rightBackMotor = new WPI_TalonSRX(2);
   CANSparkMax rightFrontMotor;
   CANSparkMax rightBackMotor;
   CANSparkMax leftFrontMotor;
@@ -40,68 +38,71 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  
+
   private static final XboxController controller = new XboxController(0);
   JoystickButton soleGreenButton = new JoystickButton(controller, Button.kA.value);
   JoystickButton soleYellowButton = new JoystickButton(controller, Button.kY.value);
-  // JoystickButton soleRedButton = new JoystickButton(controller, Button.kB.value);
-  // JoystickButton extendArm = new JoystickButton(controller, Button.kRightBumper.value);
-  // JoystickButton detractArm = new JoystickButton(controller, Button.kLeftBumper.value);
+  // JoystickButton soleRedButton = new JoystickButton(controller,
+  // Button.kB.value);
   PIDController pid;
 
-  private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid (PneumaticsModuleType.CTREPCM, 4, 5);
+  private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 5);
   private static PneumaticsControlModule pcm = new PneumaticsControlModule(0);
   // private static DoubleSolenoid soleYellow = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
   // private static DoubleSolenoid soleGreen = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
-  
-  
-
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
+    CameraServer.startAutomaticCapture();
+
     m_robotContainer = new RobotContainer();
+
     System.out.println("robotInit has been called.");
-    rightFrontMotor = new CANSparkMax(4, MotorType.kBrushed);
+
     rightBackMotor = new CANSparkMax(2, MotorType.kBrushed);
     leftFrontMotor = new CANSparkMax(3, MotorType.kBrushed);
+    rightFrontMotor = new CANSparkMax(4, MotorType.kBrushed);
     leftBackMotor = new CANSparkMax(5, MotorType.kBrushed);
-    //leftClimber = new CANSparkMax(6, MotorType.kBrushed);
+    leftClimber = new CANSparkMax(6, MotorType.kBrushed);
     rightClimber = new CANSparkMax(7, MotorType.kBrushed);
-
-
 
     leftFrontMotor.setInverted(true);
     leftBackMotor.setInverted(true);
 
-
-    
-    
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     int myInt = 1;
 
     myInt = 2;
     CommandScheduler.getInstance().run();
-    //System.out.println("robotPeriodic has been called.");
+    // System.out.println("robotPeriodic has been called.");
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
@@ -112,9 +113,13 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
     System.out.println("autonomousInit has been called.");
@@ -134,8 +139,7 @@ public class Robot extends TimedRobot {
     rightBackMotor.set(0.5);
     leftFrontMotor.set(0.5);
     leftBackMotor.set(0.5);
-   
-    
+
   }
 
   @Override
@@ -153,97 +157,86 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    //System.out.println("teleopPeriodic has been called.");
-    // If up or down button pressed then move arm up or down
-    // Look at old code and look at drivetrain
-    //
-
- // double speed = -controller.getLeftY();
-  double turn = controller.getRightX()*0.5;
-  //double strafe = controller.getLeftX();
-
-  double x = controller.getLeftX();
-  double y = controller.getLeftY();
-
-  double theta = Math.atan2(y,x);
-  double power = Math.hypot(x,y);
-  
-
-double sin = Math.sin(theta - Math.PI/4);
-double cos = Math.cos(theta - Math.PI/4);
-double max =   Math.max(Math.abs(sin), Math.abs(cos));
-
-double leftFront = power * cos/max + turn;
-double rightFront =power * sin/max - turn;
-double leftRear = power * sin/max + turn;
-double rightRear = power * cos/max - turn;
-
-if ((power + Math.abs(turn)) > 1){
-  leftFront /= power + Math.abs(turn);
-  rightFront /= power + Math.abs(turn);
-  leftRear /= power + Math.abs(turn);
-  rightRear /= power + Math.abs(turn);
-}
-
-
-  // double leftFront = speed + turn + strafe;
-  // double rightFront = speed - turn - strafe;
-  // double leftRear = speed + turn - strafe;
-  // double rightRear = speed - turn + strafe;
-
-
-
-  rightFrontMotor.set(rightFront);
-  rightBackMotor.set(rightRear);
-  leftFrontMotor.set(leftFront);
-  leftBackMotor.set(leftRear);
-
-  
-// get value for controller
-boolean leftBumper = controller.getLeftBumper();
-boolean rightBumper = controller.getRightBumper();
-int dpad = controller.getPOV(); 
-
-double climberSpeed = 0.0;
-
-if (dpad > 135 && dpad < 225) {
-// neg speed
-  climberSpeed = -0.2;
-}
-else if (dpad != -1){
-  if (dpad > 315 || dpad < 45){
-    // pos speed 
-    climberSpeed = 0.2;
-  }
-}
-else if (dpad == -1) {
-  climberSpeed = 0.0;
-}
-
-
-  if (leftBumper == true && rightBumper == true){
-    leftClimber.set(climberSpeed);
-     rightClimber.set(climberSpeed);
-  }
-  else if (leftBumper == true){
-    leftClimber.set(climberSpeed);   
-  }
-  else if (rightBumper == true){
-    rightClimber.set(climberSpeed);
-  }
-
-      if (controller.getYButtonPressed() == true){
-        m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
-      }
-      else if (controller.getAButtonPressed() == true){
-        m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse); 
-      }
+    // System.out.println("teleopPeriodic has been called.");
     
+    // method 1 for mechanum drive
+    double turn = controller.getRightX() * 0.5;
+    double strafe = controller.getLeftX();
+    double speed = controller.getLeftX();
 
+    double leftFront = speed + turn + strafe;
+    double rightFront = speed - turn - strafe;
+    double leftRear = speed + turn - strafe;
+    double rightRear = speed - turn + strafe;
+
+    rightFrontMotor.set(rightFront);
+    rightBackMotor.set(rightRear);
+    leftFrontMotor.set(leftFront);
+    leftBackMotor.set(leftRear);
+
+    // method 2 for mechanum drive
+    // double speed = -controller.getLeftY();
+    // double turn = controller.getRightX()*0.5;
+    // //double strafe = controller.getLeftX();
+
+    // double x = controller.getLeftX();
+    // double y = -controller.getLeftY();
+
+    // double theta = Math.atan2(y,x);
+    // double power = Math.hypot(x,y);
+
+    // double sin = Math.sin(theta - Math.PI/4);
+    // double cos = Math.cos(theta - Math.PI/4);
+    // double max = Math.max(Math.abs(sin), Math.abs(cos));
+
+    // double leftFront = power * cos/max + turn;
+    // double rightFront =power * sin/max - turn;
+    // double leftRear = power * sin/max + turn;
+    // double rightRear = power * cos/max - turn;
+
+    // if ((power + Math.abs(turn)) > 1){
+    // leftFront /= power + Math.abs(turn);
+    // rightFront /= power + Math.abs(turn);
+    // leftRear /= power + Math.abs(turn);
+    // rightRear /= power + Math.abs(turn);
+
+    // code for climbers
+    boolean leftBumper = controller.getLeftBumper();
+    boolean rightBumper = controller.getRightBumper();
+    int dpad = controller.getPOV();
+
+    double climberSpeed = 0.0;
+
+    if (dpad > 135 && dpad < 225) {
+      // neg speed
+      climberSpeed = -0.2;
+    } else if (dpad != -1) {
+      if (dpad > 315 || dpad < 45) {
+        // pos speed
+        climberSpeed = 0.2;
+      }
+    } else if (dpad == -1) {
+      climberSpeed = 0.0;
+    }
+
+    if (leftBumper == true && rightBumper == true) {
+      leftClimber.set(climberSpeed);
+      rightClimber.set(climberSpeed);
+    } else if (leftBumper == true) {
+      leftClimber.set(climberSpeed);
+    } else if (rightBumper == true) {
+      rightClimber.set(climberSpeed);
+    }
+
+
+    // code for piston
+    if (controller.getYButtonPressed() == true) {
+      m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+    } else if (controller.getAButtonPressed() == true) {
+      m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
 
   }
-
-
 
   @Override
   public void testInit() {
@@ -256,7 +249,7 @@ else if (dpad == -1) {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
-    //System.out.println("testPeriodic has been called.");
+    // System.out.println("testPeriodic has been called.");
 
   }
 
@@ -270,7 +263,7 @@ else if (dpad == -1) {
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {
-    //System.out.println("simulationPeriodic has been called.");
+    // System.out.println("simulationPeriodic has been called.");
 
   }
 }
